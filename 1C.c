885cd08de5deed42d0bde494eeb9f4bcddd1d304
yash12R <yashraut584@gmail.com>
2025-11-06 19:09:27 +0530

    Update 1C.c

--- 1C.c
+++ 1C.c
@@ -1,40 +1,60 @@
 #include <stdio.h>
+#include <string.h>
 
+// Structure definition
 struct Student {
+    int rollNo;
     char name[50];
-    int roll;
-    float avg;
+    int age;
+    float avgMarks;
 };
 
-int main() {
-    int n;
-    printf("Enter number of students: ");
-    scanf("%d", &n);
+// Function to perform Bubble Sort (Descending by avgMarks)
+void bubbleSort(struct Student s[], int n) {
+    struct Student temp;
 
-    struct Student s[n], temp;
-
-    // Input details
-    for (int i = 0; i < n; i++) {
-        printf("\nEnter name, roll number, and average marks of student %d: ", i + 1);
-        scanf("%s %d %f", s[i].name, &s[i].roll, &s[i].avg);
-    }
-
-    // Bubble sort (descending order by avg)
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (s[j].avg < s[j + 1].avg) {
+            if (s[j].avgMarks < s[j + 1].avgMarks) {
                 temp = s[j];
                 s[j] = s[j + 1];
                 s[j + 1] = temp;
             }
         }
     }
+}
+
+int main() {
+    struct Student s[50];
+    int n;
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter details for Student %d:\n", i + 1);
+        printf("Roll No: ");
+        scanf("%d", &s[i].rollNo);
+        printf("Name: ");
+        scanf("%s", s[i].name);
+        printf("Age: ");
+        scanf("%d", &s[i].age);
+        printf("Average Marks: ");
+        scanf("%f", &s[i].avgMarks);
+    }
+
+    // Sorting
+    bubbleSort(s, n);
 
     // Display sorted list
-    printf("\nStudents sorted by average marks (Descending):\n");
-    printf("Name\tRoll\tAverage\n");
+    printf("\nStudents sorted in descending order of Average Marks:\n");
+    printf("------------------------------------------------------\n");
+    printf("%-10s %-15s %-10s %-10s\n", "Roll No", "Name", "Age", "Avg Marks");
+    printf("------------------------------------------------------\n");
+
     for (int i = 0; i < n; i++) {
-        printf("%s\t%d\t%.2f\n", s[i].name, s[i].roll, s[i].avg);
+        printf("%-10d %-15s %-10d %-10.2f\n",
+               s[i].rollNo, s[i].name, s[i].age, s[i].avgMarks);
     }
 
     return 0;
